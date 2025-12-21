5f6ce902c456bade90e6bcfae9e435635522ec08
Ankit Raj <ankitraj100220@gmail.com>
2025-12-21 22:15:49 +0530

    auto-sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import TerminalLoader from "@/componets/loader";
+import Loader from "@/componets/loader";
 import Header from "@/componets/header";
 
 export default function HomePage() {
@@ -9,19 +9,12 @@ export default function HomePage() {
 
   return (
     <>
-      {loading && <TerminalLoader onComplete={() => setLoading(false)} />}
+      {loading && <Loader onComplete={() => setLoading(false)} />}
 
-      {/* {!loading && (
-        <main className="h-screen flex items-center justify-center">
-          <h1 className="text-4xl font-bold">App Loaded</h1>
+      <Header/>
+        <main className="min-h-screen flex items-center justify-center">
+          <h1 className="text-4xl font-bold">Main Content</h1>
         </main>
-      )} */}
-
-
-    <Header/>
-      <main className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold">Main Content</h1>
-      </main>
     </>
   );
 }
