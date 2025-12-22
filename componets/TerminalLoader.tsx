02d36e4ee78d43785ccdcc0930212e0ebbcffb7e
Ankit Raj <ankitraj100220@gmail.com>
2025-12-22 15:26:38 +0530

    auto-sync

---
+++ componets/TerminalLoader.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AsciiCanvasLoader from "./AsciiCanvasLoader";
+
+export default function TerminalLoader({ onFinish }: { onFinish: () => void }) {
+  return (
+    <div className="fixed inset-0 z-50 bg-black flex items-center justify-center">
+      <AsciiCanvasLoader done={onFinish} />
+    </div>
+  );
+}
