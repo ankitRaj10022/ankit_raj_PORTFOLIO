276e2ee7b382aa92d3889fa704b36f43e2d7a829
Ankit Raj <ankitraj100220@gmail.com>
2025-12-22 15:38:19 +0530

    auto-sync

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({children,}: {children: React.ReactNode}){
 
   useEffect(() => {
     setLoading(true);
-    const timeout = setTimeout(() => setLoading(false), 22200);
+    const timeout = setTimeout(() => setLoading(false), 3200);
     return () => clearTimeout(timeout);
   }, [pathname]);
 
