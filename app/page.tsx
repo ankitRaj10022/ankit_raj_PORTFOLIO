89e207049e75535ae44386aa5cd326f115b7d4fd
Ankit Raj <ankitraj100220@gmail.com>
2025-12-23 14:31:59 +0530

    auto-sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import Loader from "@/componet/loader";
-import Navbar from "@/componet/navBar";
+import Loader from "@/component/loader";
+import Navbar from "@/component/navBar";
 
 export default function HomePage() {
   const [loading, setLoading] = useState(true);
--- component/navBar.tsx
+++ component/navBar.tsx
@@ -53,7 +53,7 @@ const components: { title: string; href: string; description: string }[] = [
   },
 ]
 
-export function NavigationMenuDemo() {
+export function Navbar() {
   const isMobile = useIsMobile()
 
   return (
